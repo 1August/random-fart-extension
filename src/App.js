de651ae1647e159a89469d2abb9bcaab07cdac9e
1August <boktaban@gmail.com>
2022-07-12 15:46:38 +0600

    Add useAudio hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import {Switch} from "./UI/Switch/Switch";
-import {useState} from "react";
+import {Switch} from "./UI/Switch/Switch"
 
 import './App.css'
+import {useState} from "react";
 
 const App = () => {
-  const [checked, setChecked] = useState(true);
+    const [checked, setChecked] = useState(false)
 
-  return (
-      <div className="App">
-        <Switch
-            isOn={checked}
-            handleToggle={() => setChecked(!checked)}
-            colorOne="#EF476F"
-            colorTwo="#06D6A0"
-        />
-      </div>
-  )
+    return (
+        <div className="App">
+            <Switch
+                isOn={checked}
+                handleToggle={() => setChecked(!checked)}
+                colorOne="#EF476F"
+                colorTwo="#06D6A0"
+            />
+        </div>
+    )
 }
 
 export default App
